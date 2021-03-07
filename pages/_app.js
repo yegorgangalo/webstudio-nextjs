3a3048d74702b5887fbdac97c361b22c3db59a54
Yegor Gangalo <gangaloyegor@gmail.com>
2021-03-07 22:48:41 +0200

    add progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/main.scss';
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height="3"
+          options={{ showSpinner: false }}
+      />
+      <Component {...pageProps} />
+    </>
+
+  )
+
 }
